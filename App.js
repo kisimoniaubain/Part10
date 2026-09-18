e61c5d7e685a22ecb633916f6c6142863ddc00a8
Student <student@sirafrica.org>
2026-09-18 12:33:50 +0300

    Exercise 10.11 Fetch repositories with Apollo Client

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client';
 import Main from './Main';
 
+const client = new ApolloClient({
+  uri: 'http://192.168.1.225:4000',
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
-  return <Main />;
+  return (
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
+  );
 };
 
 export default App;
